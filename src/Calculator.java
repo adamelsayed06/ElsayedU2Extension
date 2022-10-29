982a79e4dffb9cb8c766169c67d966c70d5cceb1
adamelsayed06 <adame68@nycstudents.net>
2022-10-29 17:12:49 -0400

    :)

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,2 +1,41 @@
-package PACKAGE_NAME;public class Calculator {
+public class Calculator {
+
+    private double number1;
+    private double number2;
+
+    public void calc(double number1, double number2) {
+        this.number1 = number1;
+        this.number2 = number2;
+    }
+
+    public double multiply() {
+        double product = number1 * number2;
+        return product;
+    }
+
+    public double divide(){
+        double quotient = number1 / number2;
+        return quotient;
+    }
+
+    public double add(){
+        double sum = number1 + number2;
+        return sum;
+    }
+
+    public double subtract(){
+        double difference = number1 - number2;
+        return difference;
+    }
+
+    public double mod(){
+        double modResult = number1 % number2;
+        return modResult;
+    }
+
+    public double exponentiation(){
+        double exponentResult = Math.pow(number1, number2);
+        return exponentResult;
+    }
 }
+
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,71 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scan = new Scanner(System.in);
+        Calculator calc = new Calculator();
+        System.out.println("Enter an expression with *, /, +, -, %, or ^: ");
+        String expression = scan.nextLine();
+
+        if (expression.contains("*")){
+            int indexOfStar = expression.indexOf("*");
+            String num1AsAString = expression.substring(0, indexOfStar - 1);
+            String num2AsAString = expression.substring(indexOfStar + 2);
+            double num1 = Double.parseDouble(num1AsAString);
+            double num2 = Double.parseDouble(num2AsAString);
+            calc.calc(num1, num2);
+            System.out.print(calc.multiply());
+        }
+
+        if (expression.contains("/")){
+            int indexOfDivide = expression.indexOf("/");
+            String num1AsAString = expression.substring(0, indexOfDivide - 1);
+            String num2AsAString = expression.substring(indexOfDivide + 2);
+            double num1 = Double.parseDouble(num1AsAString);
+            double num2 = Double.parseDouble(num2AsAString);
+            calc.calc(num1, num2);
+            System.out.print(calc.divide());
+        }
+
+        if (expression.contains("+")){
+            int indexOfPlus = expression.indexOf("+");
+            String num1AsAString = expression.substring(0, indexOfPlus - 1);
+            String num2AsAString = expression.substring(indexOfPlus + 2);
+            double num1 = Double.parseDouble(num1AsAString);
+            double num2 = Double.parseDouble(num2AsAString);
+            calc.calc(num1, num2);
+            System.out.print(calc.add());
+        }
+
+        if (expression.contains("-")){
+            int indexOfSubtract = expression.indexOf("-");
+            String num1AsAString = expression.substring(0, indexOfSubtract - 1);
+            String num2AsAString = expression.substring(indexOfSubtract + 2);
+            double num1 = Double.parseDouble(num1AsAString);
+            double num2 = Double.parseDouble(num2AsAString);
+            calc.calc(num1, num2);
+            System.out.print(calc.subtract());
+        }
+
+        if (expression.contains("%")){
+            int indexOfMod = expression.indexOf("%");
+            String num1AsAString = expression.substring(0, indexOfMod - 1);
+            String num2AsAString = expression.substring(indexOfMod + 2);
+            double num1 = Double.parseDouble(num1AsAString);
+            double num2 = Double.parseDouble(num2AsAString);
+            calc.calc(num1, num2);
+            System.out.print(calc.mod());
+        }
+
+        if (expression.contains("^")){
+            int indexOfExponent = expression.indexOf("^");
+            String num1AsAString = expression.substring(0, indexOfExponent - 1);
+            String num2AsAString = expression.substring(indexOfExponent + 2);
+            double num1 = Double.parseDouble(num1AsAString);
+            double num2 = Double.parseDouble(num2AsAString);
+            calc.calc(num1, num2);
+            System.out.print(calc.exponentiation());
+        }
+
     }
 }
